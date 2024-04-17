b7b4d345e1099e5a00ed7ac4c3b392e2fa8f2ed5
sifad-hossain <niaz.sifad@gmail.com>
2024-04-17 21:10:12 +0600

    redirects file added

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,9 +1,14 @@
+import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 
 
 const NotFound = () => {
     return (
         <div className='text-center mt-48 bg-slate-400 w-[500px] mx-auto '>
+
+            <Helmet>
+                <title>DreamDwellings | 404 Page Not Found</title>
+            </Helmet>
             <div className="py-10">
                 <h1 className='text-4xl font-bold text-black '>
                     404
@@ -15,7 +20,7 @@ const NotFound = () => {
             </div>
             <div className="pt-10 bg-white shadow-lg hover:scale-105 hover:bg-slate-200">
 
-                <Link className="  btn-secondary text-7xl text-black font-bold hover:text-blue-700">GO Back</Link>
+                <Link className="  btn-secondary text-7xl text-black font-bold hover:text-blue-700">Back to Home</Link>
             </div>
         </div>
     );
